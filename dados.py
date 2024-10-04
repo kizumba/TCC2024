30fab96b83645e5bb828eebdebc1002f27fda84d
kizumba <12.25andre@gmail.com>
2024-10-04 14:34:56 -0300

    Dados falsos para simular sistema

---
+++ dados.py
@@ -0,0 +1,41 @@
+assento = {
+    'fileira':'',
+    'numero':None,
+    'livre':True
+}
+
+ingresso = {
+    'preco':None
+}
+
+eventos = [
+    {
+        'id':1,
+        'nome':'Univesp de neve e os oito alunos ',
+        'descricao':'Alunos desesperados tentando se formar em engenharia.',
+        'data':'31/12/2024',
+        'hora_inicio':'21:00'
+    },
+    {
+        'id':2,
+        'nome':'Hermes e Renato',
+        'descricao':'Show do grupo Hermes e Renato',
+        'data':'30/11/2024',
+        'hora_inicio':'20:00'
+    },  
+    {
+        'id':3,
+        'nome':'Elvis',
+        'descricao':'Peça magistral mostrando o renascimento do rei.',
+        'data':'30/10/2024',
+        'hora_inicio':'22:00'
+    },
+    {
+        'id':4,
+        'nome':'Fracasso',
+        'descricao':'Quando vencer é um obstáculo.',
+        'data':'01/01/2025',
+        'hora_inicio':'13:00'
+    }
+]
+
--- routes.py
+++ routes.py
@@ -1,16 +1,24 @@
 from app import app
 from flask import render_template
 #from models import *
+from dados import *
+
+# eventos = [evento1, evento2, evento3]
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',eventos=eventos)
 
-@app.route('/evento')
-def evento():
-    return render_template('evento.html')
+@app.route('/evento/<int:id>')
+def evento(id):
+    
+    return render_template('evento.html',id=id)
 
-@app.route('/assento/<fileira>/<int:j>')
-def assento(fileira,j):
-    return render_template('assento.html',fileira=fileira, j=j)
+@app.route('/assento/<fileira>/<int:j>/<int:id>')
+def assento(fileira,j,id):
+    return render_template('assento.html',fileira=fileira, j=j,id=id)
+
+@app.route('/venda')
+def ingresso():
+    return render_template('venda.html')
